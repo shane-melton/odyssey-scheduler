4ddcc955edd64e2d1db155e19f2d1bde0a96954c
Shane Melton <smelton@nevada.unr.edu>
2018-07-11 02:04:14 -0700

    Fixed more routing issues...

--- src/server/main.ts
+++ src/server/main.ts
@@ -13,8 +13,17 @@ import { join } from 'path';
 import { FOLDER_CLIENT, FOLDER_DIST } from '@shared/constants';
 
 import { ApplicationModule } from './server.module';
+import { Catch, ExceptionFilter, HttpException, NotFoundException } from '@nestjs/common';
+import { Response } from 'express';
 
 const app = express();
+
+@Catch(NotFoundException)
+class NotFoundExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, response: Response) {
+    response.sendFile(join(FOLDER_CLIENT, 'index.html'));
+  }
+}
 
 async function bootstrap() {
   if (process.env.NODE_ENV === 'production') {
@@ -23,11 +32,7 @@ async function bootstrap() {
 
   const server = await NestFactory.create(ApplicationModule, app, {});
 
-  if (process.env.NODE_ENV === 'production') {
-    app.get('*', (req, res) => {
-      res.sendFile(join(FOLDER_CLIENT, 'index.html'));
-    });
-  }
+  server.useGlobalFilters(new NotFoundExceptionFilter());
 
   await server.listen(process.env.PORT || 5400);
 }
